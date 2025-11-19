9596d26a1976252689ae87033f3165ddd2cd0480
Basile Andre <basile.andre056@gmail.com>
2025-11-19 10:48:12 +0000

    ajout de to_do.md instructions pour déploiment du module

--- backend/gn_module_quadrige/blueprint.py
+++ backend/gn_module_quadrige/blueprint.py
@@ -4,7 +4,8 @@ from flask import Blueprint
 from .routes import init_routes
 
 # Nom = MODULE_CODE, url_prefix = chemin sous l’API GN
-blueprint = Blueprint("quadrige", __name__, url_prefix="/quadrige")
+#peut être qu'il faudra supprimer le préfixe /quadrige plus tard :: , url_prefix="/quadrige"
+blueprint = Blueprint("quadrige", __name__)
 
 # Enregistrement des routes définies dans routes.py
 init_routes(blueprint)
--- backend/gn_module_quadrige/config_schema_toml.py
+++ backend/gn_module_quadrige/config_schema_toml.py
@@ -1,4 +1,4 @@
-# backend/gn_module_quadrige/conf_schema_toml.py
+# backend/gn_module_quadrige/config_schema_toml.py
 """
 Schéma Marshmallow pour la configuration TOML du module Quadrige.
 
@@ -6,6 +6,17 @@ La classe doit impérativement s'appeler GnModuleSchemaConf.
 Les valeurs réelles seront surchargées par config/config_gn_module.toml
 dans l’installation GeoNature.
 """
+
+# Important pour la PROD :
+
+#Le fichier utilisé sur le serveur ne sera pas ton module_code_config.toml, mais le fichier central de GN (souvent config/config_gn_module.toml ou équivalent, géré par l’admin GN).
+#
+#Dedans, il faudra une section :
+#
+#[quadrige]
+#graphql_url = "https://quadrige-core.ifremer.fr/graphql/public"
+#access_token = "TON_TOKEN_DE_PROD"
+
 
 from marshmallow import Schema, fields
 
